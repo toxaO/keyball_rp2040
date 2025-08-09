1933ea57276a18aa58b236284c20a719ca2e128e
toxa_O <material.xyz.44@gmail.com>
2025-08-10 08:23:29 +0900

    led off

--- keyball/keyball39/rules.mk
+++ keyball/keyball39/rules.mk
@@ -46,11 +46,11 @@ SRC += lib/oledkit/oledkit.c    # OLED utility for Keyball series.
 SRC += lib/keyball/keyball.c
 
 # Include my library
-SRC += mylib/features/layer.c
-SRC += mylib/features/macro_key.c
-SRC += mylib/features/oled.c
-SRC += mylib/features/swipe.c
-SRC += mylib/features/util.c
+# SRC += mylib/features/layer.c
+# SRC += mylib/features/macro_key.c
+# SRC += mylib/features/oled.c
+# SRC += mylib/features/swipe.c
+# SRC += mylib/features/util.c
 
 # Disable other features to squeeze firmware size
 SPACE_CADET_ENABLE = no
--- keyball/mylib/features/layer.c
+++ keyball/mylib/features/layer.c
@@ -34,42 +34,42 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 7);
 
-    //LED------------------------------
-    uint8_t layer = biton32(state);
-    switch (layer) {
-        case _Def:
-            rgblight_sethsv(HSV_CYAN);
-            rgblight_mode(RGBLIGHT_MODE_BREATHING + 0);
-            break;
-        case _Sym:
-            rgblight_sethsv(HSV_PURPLE);
-            rgblight_mode(RGBLIGHT_MODE_BREATHING + 3);
-            break;
-        case _mCur:
-            rgblight_sethsv(HSV_GREEN);
-            rgblight_mode(RGBLIGHT_MODE_BREATHING + 3);
-            break;
-        case _wCur:
-            rgblight_sethsv(HSV_GREEN);
-            rgblight_mode(RGBLIGHT_MODE_BREATHING + 3);
-            break;
-        case _mMou:
-            rgblight_sethsv(HSV_BLUE);
-            rgblight_mode(RGBLIGHT_MODE_BREATHING + 2);
-            break;
-        case _wMou:
-            rgblight_sethsv(HSV_BLUE);
-            rgblight_mode(RGBLIGHT_MODE_BREATHING + 2);
-            break;
-        case _NumP:
-            rgblight_sethsv(HSV_ORANGE);
-            rgblight_mode(RGBLIGHT_MODE_BREATHING + 0);
-            break;
-        case _Scr:
-            rgblight_sethsv(HSV_BLUE);
-            rgblight_mode(RGBLIGHT_MODE_SNAKE + 2);
-            break;
-    }
+    ////LED------------------------------
+    //uint8_t layer = biton32(state);
+    //switch (layer) {
+    //    case _Def:
+    //        rgblight_sethsv(HSV_CYAN);
+    //        rgblight_mode(RGBLIGHT_MODE_BREATHING + 0);
+    //        break;
+    //    case _Sym:
+    //        rgblight_sethsv(HSV_PURPLE);
+    //        rgblight_mode(RGBLIGHT_MODE_BREATHING + 3);
+    //        break;
+    //    case _mCur:
+    //        rgblight_sethsv(HSV_GREEN);
+    //        rgblight_mode(RGBLIGHT_MODE_BREATHING + 3);
+    //        break;
+    //    case _wCur:
+    //        rgblight_sethsv(HSV_GREEN);
+    //        rgblight_mode(RGBLIGHT_MODE_BREATHING + 3);
+    //        break;
+    //    case _mMou:
+    //        rgblight_sethsv(HSV_BLUE);
+    //        rgblight_mode(RGBLIGHT_MODE_BREATHING + 2);
+    //        break;
+    //    case _wMou:
+    //        rgblight_sethsv(HSV_BLUE);
+    //        rgblight_mode(RGBLIGHT_MODE_BREATHING + 2);
+    //        break;
+    //    case _NumP:
+    //        rgblight_sethsv(HSV_ORANGE);
+    //        rgblight_mode(RGBLIGHT_MODE_BREATHING + 0);
+    //        break;
+    //    case _Scr:
+    //        rgblight_sethsv(HSV_BLUE);
+    //        rgblight_mode(RGBLIGHT_MODE_SNAKE + 2);
+    //        break;
+    //}
 
     return state;
 }
